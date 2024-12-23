71be6b7b6b6f72d0a231835c9d8331d884e892e1
Josh Monson <josh.monson@cosm.com>
2024-12-23 08:27:16 -0700

    Adding in sorting and filtering

--- app/api/v1/tasks.py
+++ app/api/v1/tasks.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from fastapi import APIRouter, HTTPException
 from typing import List, Optional
 from fastapi.params import Depends
 from pydantic import BaseModel
+from sqlalchemy import case
 from sqlalchemy.orm import Session
 from app.auth import get_current_user
 from app.database import get_db, User, Task
@@ -27,13 +28,23 @@ class TaskResponse(TaskBase):
     class ConfigDict:
         from_attributes = True
 
+SORT_KEYS = ['date_due', 'date_added', 'date_completed', 'priority']
+SORT = {
+    'priority': case((Task.priority == 0, 7), else_=Task.priority)
+}
+
 
 @router.get("/", response_model=List[TaskResponse])
-async def read_tasks(skip: int = 0, limit: int = 100, include_completed: bool = False, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+async def read_tasks(sort: str = '', filter_name: str = '', skip: int = 0, limit: int = 100, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     query = db.query(Task).filter(Task.owner_id == current_user.id)
-    if not include_completed:
-        query = query.filter(Task.date_completed == None)
-    tasks = query.order_by(Task.date_added).offset(skip).limit(limit).all()
+    if sort in SORT_KEYS:
+        if sort in SORT:
+            query = query.order_by(SORT[sort])
+        else:
+            query = query.order_by(getattr(Task, sort))
+    query = apply_filter(query, filter_name)
+
+    tasks = query.offset(skip).limit(limit).all()
     return tasks
 
 @router.post("/", response_model=TaskResponse)
@@ -70,3 +81,39 @@ async def delete_task(task_id: int, current_user: User = Depends(get_current_use
     db.delete(db_task)
     db.commit()
     return {"message": "Task deleted"}
+
+
+def apply_filter(query, filter_name):
+    if filter_name == 'today':
+        now = datetime.now(timezone.utc)
+        end_of_day = now.replace(hour=23, minute=59, second=59, microsecond=0)
+
+        query = query.filter(Task.date_due != None)
+        query = query.filter(Task.date_due <= end_of_day)
+
+    elif filter_name == 'week':
+        now = datetime.now(timezone.utc)
+        start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_of_week = start_of_day + timedelta(days=7)
+
+        query = query.filter(Task.date_due != None)
+        query = query.filter(Task.date_due <= end_of_week)
+
+    elif filter_name == 'high_priority':
+        query = query.filter(Task.priority > 0).filter(Task.priority <= 3)
+
+    elif filter_name.startswith('category:'):
+        category = filter_name[9:]
+        query = query.filter(Task.categories.any(category))
+
+    elif filter_name.startswith('location:'):
+        location = filter_name[9:]
+        query = query.filter(Task.location == location)
+
+    elif filter_name == 'completed':
+        query = query.filter(Task.date_completed != None)
+
+    if filter_name != 'completed':
+        query = query.filter(Task.date_completed == None)
+
+    return query
